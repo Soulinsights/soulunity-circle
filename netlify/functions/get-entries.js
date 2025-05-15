ac04aeb355e0836f7ea1a74b095e15e356185a6c
Soulinsights <seekwithin82@gmail.com>
2025-05-15 21:58:30 +0200

    get-entries.js

--- netlify/functions/get-entries.js
+++ netlify/functions/get-entries.js
@@ -1,27 +1,13 @@
-
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async () => {
-  const filePath = path.join(__dirname, "../../data/submissions.json");
+  const filePath = path.join(__dirname, '../../data/submissions.json');
 
   try {
-    if (fs.existsSync(filePath)) {
-      const data = fs.readFileSync(filePath);
-      return {
-        statusCode: 200,
-        body: data.toString(),
-      };
-    } else {
-      return {
-        statusCode: 200,
-        body: "[]",
-      };
-    }
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Failed to read entries" }),
-    };
+    const data = fs.existsSync(filePath) ? fs.readFileSync(filePath) : '[]';
+    return { statusCode: 200, body: data.toString() };
+  } catch (e) {
+    return { statusCode: 500, body: JSON.stringify({ error: e.message }) };
   }
 };
